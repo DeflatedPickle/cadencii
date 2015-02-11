062f54fd3ef6f71ce1eed6c73d944e7f70037618
Atsushi Eno <atsushieno@gmail.com>
2015-02-11 17:22:05 +0800

    add missing ListView implementation types.

---
+++ src/Cadencii.Gui.Framework.Xwt/toolkit/ListViewColumnImpl.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Cadencii.Gui.Toolkit
+{
+	public class ListViewColumnImpl : UiListViewColumn
+	{
+		string text;
+		Xwt.ListViewColumn impl;
+
+		#region UiListViewColumn implementation
+
+		object UiListViewColumn.Native {
+			get { return impl; }
+			set {
+				impl = (Xwt.ListViewColumn) value;
+				impl.Title = text;
+			}
+		}
+
+		// ignore. No way to set it.
+		int UiListViewColumn.Width { get; set; }
+
+		string UiListViewColumn.Text {
+			get { return text; }
+			set {
+				text = value;
+				if (impl != null)
+					impl.Title = value;
+			}
+		}
+
+		#endregion
+	}
+}
+
---
+++ src/Cadencii.Gui.Framework.Xwt/toolkit/ListViewItemImpl.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Cadencii.Gui.Toolkit
+{
+	public class ListViewItemImpl : UiListViewItem
+	{
+		#region UiListViewItem implementation
+
+		List<ListViewItemImpl> items = new List<ListViewItemImpl> ();
+
+		UiListViewItem UiListViewItem.GetSubItem (int i)
+		{
+			return items [i];
+		}
+
+		void UiListViewItem.AddSubItem (string subText)
+		{
+			items.Add (new ListViewItemImpl () { Text = subText });
+		}
+
+		// FIXME: maybe we need ListStore(ListView.DataSource) to "implement" those properties, if ever possible.
+		object UiListViewItem.Native { get; set; }
+
+		public string Text { get; set; }
+
+		Color UiListViewItem.BackColor { get; set; }
+
+		bool UiListViewItem.Checked { get; set; }
+
+		bool UiListViewItem.Selected { get; set; }
+		#endregion
+	}
+}
+
